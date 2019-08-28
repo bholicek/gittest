b844b5fd8bd6e769e74fe43dc617278d225b4b42
Unknown <madsonb@gmatc.matc.edu>
2019-08-28 16:31:58 -0500

    Ben
    Did this work?

--- Assets/testscript.cs
+++ Assets/testscript.cs
@@ -7,7 +7,7 @@ public class testscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Ben, does this work?");
     }
 
     // Update is called once per frame
